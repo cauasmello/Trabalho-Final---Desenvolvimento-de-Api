8a159a1e85a5acf7ea92bbfd8237540d03fb26fa
Cauã Stulpen Mello <102328667+cauasmello@users.noreply.github.com>
2022-05-31 17:17:48 -0300

    Primeiro commit.

---
+++ eccomerce/src/main/java/com/example/eccomerce/EccomerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceApplication.class, args);
+	}
+
+}
