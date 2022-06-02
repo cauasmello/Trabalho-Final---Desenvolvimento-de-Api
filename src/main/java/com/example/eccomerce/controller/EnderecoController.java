d7471ec1b5496a2ae0042be37d3d93ae1769d965
Vinicius da Conceição Teixeira <viniciuscteixeira.rj@gmail.com>
2022-06-02 20:11:25 -0300

    fix Endereco

--- src/main/java/com/example/eccomerce/controller/EnderecoController.java
+++ src/main/java/com/example/eccomerce/controller/EnderecoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/endereco")
-public class ControllerEndereco {
+public class EnderecoController {
 
     @Autowired
     EnderecoService service;
