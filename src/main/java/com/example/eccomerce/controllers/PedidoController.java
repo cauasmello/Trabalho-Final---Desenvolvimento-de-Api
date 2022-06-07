cdfdb3a46e52a4351b8bc6e5bce2090a61774076
AmandaAlbernaz <102327725+AmandaAlbernaz@users.noreply.github.com>
2022-06-06 22:29:54 -0300

    pedidos

---
+++ src/main/java/com/example/eccomerce/controllers/PedidoController.java
@@ -0,0 +1,61 @@
+package com.example.eccomerce.controllers;
+
+import com.example.eccomerce.exceptions.ErrorException;
+import com.example.eccomerce.models.PedidoModel;
+import com.example.eccomerce.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categoria")
+public class PedidoController {
+
+    @Autowired
+    PedidoService service;
+
+    @GetMapping
+    public ResponseEntity<List<PedidoModel>> getAll(@RequestHeader(required = true, name = "Authorization") String token) throws ErrorException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Success", "Lista de pedidos");
+
+        return new ResponseEntity<>(service.getAll(token), headers, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{numero}")
+    public ResponseEntity<PedidoModel> get(@PathVariable String numero, @RequestHeader(required = true, name = "Authorization") String token) throws ErrorException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Success", "Pedido");
+
+        return new ResponseEntity<>(service.get(numero, token), headers, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoModel> add(@RequestHeader(required = true, name = "Authorization") String token) throws ErrorException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Success", "Adicionado");
+
+        return new ResponseEntity<>(service.add(token), headers, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/{numero}")
+    public ResponseEntity<Void> put(@PathVariable String numero, @RequestHeader(required = true, name = "Authorization") String token) throws ErrorException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Success", "finalizado");
+
+        return new ResponseEntity<>(service.put(numero, token), headers, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{numero}")
+    public ResponseEntity<Void> delete(@PathVariable String numero, @RequestHeader(required = true, name = "Authorization") String token) throws ErrorException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Success", "Deletado");
+
+        return new ResponseEntity<>(service.delete(numero, token), headers, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/eccomerce/services/PedidoService.java
@@ -0,0 +1,114 @@
+package com.example.eccomerce.services;
+
+import com.example.eccomerce.exceptions.ErrorException;
+import com.example.eccomerce.models.PedidoModel;
+import com.example.eccomerce.models.UserModel;
+import com.example.eccomerce.repositories.PedidoRepository;
+import com.example.eccomerce.resources.ToolsResource;
+import com.example.eccomerce.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    PedidoRepository repositorio;
+
+    @Autowired
+    JWTUtil jwtUtil;
+
+    @Autowired
+    ToolsResource tools;
+
+    public List<PedidoModel> getAll(String token) throws ErrorException {
+        UserModel myUser = jwtUtil.getLoggedUser(token);
+        tools.existUser(myUser);
+
+        if(myUser.getRole() == 0){
+            return myUser.getCliente().getPedidos();
+        }
+
+        return repositorio.findAll();
+    }
+
+    public PedidoModel get(String numero, String token) throws ErrorException {
+        UserModel myUser = jwtUtil.getLoggedUser(token);
+        tools.existUser(myUser);
+
+        Optional<PedidoModel> optional;
+        if(myUser.getRole() == 0) {
+            optional = repositorio.findByClienteAndNumero(myUser.getCliente(), numero);
+        } else{
+            optional = repositorio.findByNumero(numero);
+        }
+
+        if (optional.isEmpty()) {
+            throw new ErrorException("Pedido não existe!");
+        }
+
+        return optional.get();
+    }
+
+    public PedidoModel add(String token) throws ErrorException {
+        UserModel myUser = jwtUtil.getLoggedUser(token);
+        tools.existUser(myUser);
+        tools.onlyClientes(myUser);
+
+        if(repositorio.existPedidoAberto(myUser.getCliente()) > 0){
+            throw new ErrorException("Você já tem um pedido aberto!");
+        }
+
+        PedidoModel novoPedido = new PedidoModel(myUser.getCliente());
+        repositorio.save(novoPedido);
+
+        return novoPedido;
+    }
+
+    public Void put(String numero, String token) throws ErrorException {
+        UserModel myUser = jwtUtil.getLoggedUser(token);
+        tools.existUser(myUser);
+        tools.onlyClientes(myUser);
+
+        Optional<PedidoModel> optional = repositorio.findByClienteAndNumero(myUser.getCliente(), numero);
+        if (optional.isEmpty()) {
+            throw new ErrorException("Pedido não existe!");
+        }
+
+        PedidoModel pedido = optional.get();
+
+        if(pedido.getProdutos() == null){
+            throw new ErrorException("Para finalizar adicione um produto!");
+        }
+
+        pedido.setEntrege(new Date());
+        pedido.setStatus("Entregue");
+        repositorio.save(pedido);
+        return null;
+    }
+
+    public Void delete(String numero, String token) throws ErrorException {
+        UserModel myUser = jwtUtil.getLoggedUser(token);
+        tools.existUser(myUser);
+
+        Optional<PedidoModel> optional;
+        if(myUser.getRole() == 0) {
+            optional = repositorio.findByClienteAndNumero(myUser.getCliente(), numero);
+        } else{
+            optional = repositorio.findByNumero(numero);
+        }
+
+        if (optional.isEmpty()) {
+            throw new ErrorException("Pedido não existe!");
+        }
+
+        PedidoModel pedido = optional.get();
+
+        repositorio.deleteById(pedido.getId());
+        return null;
+    }
+}
